ecd9f3c674a6a9994e0b84518a40c7b355950c98
HMT <82308907+HMT2002@users.noreply.github.com>
2023-05-24 09:29:22 +0700

    allow CORS

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.use((req, res, next) => {
   next();
 });
 
+
+app.use((req, res, next) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 //ROUTES
 const defaultRoute = require('./routes/defaultRoute');
 const threadRouter = require('./routes/threadRoute');
