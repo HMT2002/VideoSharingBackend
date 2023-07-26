270bf5cb2843a9e4f6f66f33376f97b2b342485f
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2023-07-26 13:52:45 +0700

    initial streaming server

--- server.js
+++ server.js
@@ -7,9 +7,53 @@ const dbVideoSharing = require('./config/database/db_index');
 
 dbVideoSharing.connect();
 
+const hls = require('hls-server');
+const fs = require('fs');
+
 //console.log(process.env);
 //START SERVER
 const port = process.env.PORT || 9000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('App listening to ' + port);
+});
+
+new hls(server, {
+  provider: {
+    exists: (req, cb) => {
+      req.url = decodeURIComponent(req.url);
+
+      console.log('server js exists' + req.url);
+      // req.url=decodeURIComponent(req.url);
+      const ext = req.url.split('.')[1];
+      const urlAndFilename = req.url.split('.')[0];
+      if (ext !== 'm3u8' && ext !== 'ts') {
+        console.log('not manifest or segment file');
+        return cb(null, true);
+      }
+
+      fs.access(__dirname + req.url, fs.constants.F_OK, function (err) {
+        if (err) {
+          console.log(__dirname + req.url);
+          console.log(err);
+          return cb(null, false);
+        }
+        cb(null, true);
+      });
+    },
+    getManifestStream: (req, cb) => {
+      req.url = decodeURIComponent(req.url);
+
+      console.log('server js getManifestStream ' + req.url);
+
+      const stream = fs.createReadStream(__dirname + req.url);
+      cb(null, stream);
+    },
+    getSegmentStream: (req, cb) => {
+      req.url = decodeURIComponent(req.url);
+
+      console.log('server js getSegmentStream ' + req.url);
+      const stream = fs.createReadStream(__dirname + req.url);
+      cb(null, stream);
+    },
+  },
 });
